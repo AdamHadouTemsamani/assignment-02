917d421570e12ae851d4a04bf41dc4062a67cf83
Adam Hadou Temsamani <ahad@itu.dk>
2022-09-19 18:38:18 +0200

    Added Tests to DelegatesTest.cs
    Have not been able to test them yet

--- Assignment2.Tests/DelegatesTests.cs
+++ Assignment2.Tests/DelegatesTests.cs
@@ -1,11 +1,52 @@
 namespace Assignment2.Tests;
 
+using System.Reflection;
+
 public class DelegatesTests
 {
-    [Fact]
-    public void Check_If__Reverse_Returns__retcarach()
+
+    public void Check_If_Reverse_Prints__retcarahc()
     {
+        // Arrange
+            string input = "character";
+            string answer = "retcarahc";
+
+            using var writer = new StringWriter();
+            Console.SetOut(writer);
+
+        // Act
+            Delegates.reverseString(input);
+
+            var program = Assembly.Load(nameof(Assignment2.Tests));
+            program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>()});
+
+        // Assert
+            var output = writer.GetStringBuilder().ToString().TrimEnd();
+            Assert.Equal(answer, output);
+    }
+
+    public void Check_If_Product_Returns_30()
+    {
+        // Arrange
+            decimal value1 = 6, value2 = 5, answer = 30;
+
+        // Act
+            var product = Delegates.product(value1,value2);
+            
+        // Assert
+            Assert.Equal(answer, product);
+    }
+
+    public void Check_If_NumericallyEquals_Returns_True()
+    {
+        // Arrange
+            int value = 50;
+            string stringValue = "0050"; 
+            
+        // Act & Assert
+            Assert.True(Delegates.numericallyEquals(value, stringValue));
 
     }
+
 
 }
